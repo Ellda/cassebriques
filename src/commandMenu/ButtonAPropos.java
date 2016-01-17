d1791b7c6982b882977b89668978edd1d0164dcb
Ellda <antoine.fonfria@hotmail.fr>
2016-01-17 23:51:27 +0100

    Equipes version dans l'a propos

--- src/commandMenu/ButtonAPropos.java
+++ src/commandMenu/ButtonAPropos.java
@@ -22,23 +22,28 @@ import javax.swing.JOptionPane;
 public class ButtonAPropos extends Command { 
    private static String text = 
 		    "<html><h1><span style='font-size: medium;'>&Eacute;quipe de d&eacute;veloppement</span></h1>" +
-		    "<ul>" + 
-			"<li><span style='color: #333399;'><strong>Adrien Burel</strong></span></li>" + 
+		    "<h2><span style='font-size: medium;'>Version 1.0</span></h2>" +
+		    "<ul>" +  
 			"<li><span style='color: #333399;'><strong>Antoine Fonfria</strong></span></li>" + 
-			"<li><span style='color: #333399;'><strong>Anthony Demuylder</strong></span></li>" + 
+			"<li><span style='color: #333399;'><strong>Adrien Burel</strong></span></li>" +
 			"<li><span style='color: #333399;'><strong>Baptiste Jaeckert</strong></span></li>" +
-			"<li><span style='color: #333399;'><strong>Benoit Fauque</strong></span></li>" + 
 			"<li><span style='color: #333399;'><strong>Dj&eacute;n&eacute;bou Monique Dembele</strong></span></li>" + 
 			"<li><span style='color: #333399;'><strong>Elliott G&eacute;mus-Pr&eacute;vost</strong></span></li>" + 
 			"<li><span style='color: #333399;'><strong>Florent Gargot</strong></span></li>" +
 			"<li><span style='color: #333399;'><strong>J&eacute;r&eacute;my Collard</strong></span></li>" + 
 			"<li><span style='color: #333399;'><strong>Jonathan Diaz-Muy</strong></span></li>" + 
-			"<li><span style='color: #333399;'><strong>Julien Mithra</strong></span></li>" + 
 			"<li><span style='color: #333399;'><strong>Landry Modeste Goutondji</strong></span></li>" + 
 			"<li><span style='color: #333399;'><strong>Mentor Bajraktari</strong></span></li>" + 
-			"<li><span style='color: #333399;'><strong>Nicolas heldmaier</strong></span></li>" + 
 			"<li><span style='color: #333399;'><strong>Olivier Scheffler</strong></span></li>" + 
 			"<li><span style='color: #333399;'><strong>Samuel Arseneault</strong></span></li>" + 
+			"</ul>" +
+		    "<h2><span style='font-size: medium;'>Version 2.0</span></h2>" +
+			"<ul>" + 
+			"<li><span style='color: #333399;'><strong>Antoine Fonfria</strong></span></li>" + 
+			"<li><span style='color: #333399;'><strong>Anthony Demuylder</strong></span></li>" + 
+			"<li><span style='color: #333399;'><strong>Benoit Fauque</strong></span></li>" + 
+			"<li><span style='color: #333399;'><strong>Julien Mithra</strong></span></li>" + 
+			"<li><span style='color: #333399;'><strong>Nicolas heldmaier</strong></span></li>" +
 			"</ul>";					
    
 	@Override
